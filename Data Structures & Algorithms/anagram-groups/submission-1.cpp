b00d9f4a2ae797e2ee84f872eff1838015d7a795
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2026-09-25 18:47:40 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+        for (auto& word: strs) {
+            string chars(26, '\0');
+            for (const auto c: word)
+                ++chars[c - 'a'];
+            map[chars].push_back(move(word));
+        }
+
+        vector<vector<string>> ans;
+        for (auto& [key, value]: map)
+            ans.push_back(move(value));
+
+        return ans;
+    }
+};
